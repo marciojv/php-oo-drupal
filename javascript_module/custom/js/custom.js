7eb74d89d9ad0b2bd7b54c262043fa9b2fd68aad
Marcio Junior Vieira <marcio@ambientelivre.com.br>
2022-12-15 13:17:30 -0300

    a

---
+++ javascript_module/custom/js/custom.js
@@ -0,0 +1,25 @@
+//console.log("Test");
+
+(function () {
+  'use strict';
+
+  // Put here your custom JavaScript code.
+  console.log ("Test");
+
+  window.confirm("Voce quer continuar?");
+  window.alert(5+6);
+  console.log("Continuando...");
+  myFunction();
+  console.log("Finalizado , tchau!");
+
+ function myFunction() {
+   console.log("Executado Myfunction");
+ }
+
+ let element = document.getElementsByClassName("title page-title");
+ console.log(element);
+
+})();
+
+
+
